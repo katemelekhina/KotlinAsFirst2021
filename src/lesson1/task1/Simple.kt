158f068619780d66dfc4a75ac87b45bb0790d888
katem <katemelekhina03@mail.ru>
2021-09-20 00:27:52 +0300

    Решены все задачи, кроме одной (вывод значения угла в радианах)

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -75,7 +75,7 @@ fun seconds(hours: Int, minutes: Int, seconds: Int): Int = hours * 3600 + minute
  * 1 сажень = 3 аршина = 48 вершков, 1 вершок = 4.445 см.
  */
 fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double =
-    (sagenes * 48 + arshins * 16 + vershoks) * 0.04445
+    (sagenes * 48 + arshins * 16 + vershoks) * 4.445 * 0.01
 
 /**
  * Тривиальная (1 балл)
